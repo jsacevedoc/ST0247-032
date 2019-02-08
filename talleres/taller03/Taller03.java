dc372f495d4cdecf3930cf7b99ef4593c45461af
ManuelG28 <36460775+ManuelG28@users.noreply.github.com>
2019-02-08 10:11:23 -0500

    Update Taller03.java

--- talleres/taller03/Taller03.java
+++ talleres/taller03/Taller03.java
@@ -1,74 +1,67 @@
-package taller03;
+package taller3;
 
 /**
  *
- * @author cl18417
+ * @author Manuel Gutierrez, Jhonatan Acevedo
  */
-public class Taller03 {
+
+ 
+public class taller3v2 {
 
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        nReinasAux(8);
+        
     }
     
     private static boolean nReinas(int n, int [ ] tablero, int c){
-    
-        if (c == n){
+        if (c >= n ){
             imprimirTablero(tablero);
-            return true;
-        }       
-        else{
-            for(int r = 0; r < n; r++){
-                
-                tablero[c] = r;
-                
-                
-            
-                if(!seAtacanHastaElIoNo(tablero, r)){
-                    
-                }
-                else{
-                    
-                }
-                 
-                if(!seAtacanHastaElIoNo(tablero, c)){
-                    return false;
-                }
-                else{
-                    nReinas(n, tablero, c+1);       
-                }
-            }
-         }
-     
-          
-    }
+            return true;          
+        }   
+        for(int r = 0; r < n; r++){
+            tablero[c] = r;  
+            if(seAtacanHastaElIoNo(tablero, c)){
+              imprimirTablero(tablero);
+              if(nReinas(n, tablero, c+1) == true){
+                return true;
+              }
+             }
+          }
+       return false;
+    }   
     
     public static void nReinasAux(int n){
-    
-    nReinas(n, new int[n], 0);
-    
+    nReinas(n, new int[n], 0); 
     }
     
     private static boolean seAtacanHastaElIoNo(int [ ] tablero, int elI){
-        for(int i = 0; i < elI-1; i++){
-            for(int j = i+1; j < elI; j++){
-                return false;
+        for(int i = 0; i <= elI-1; i++){
+            for(int j = i+1; j <= elI; j++){
+                if((Math.abs(tablero[i]-tablero[j])==Math.abs(i-j))||(tablero[i]==tablero[j])){
+                    return false;
+                }
             }
         }
         return true;
-        
-        
-
-
-
-
     }
 
-    private static void imprimirTablero(int[] tablero) {
-       
-    }
+ public static void imprimirTablero(int[] tablero) {
+    int n = tablero.length;
+    System.out.print("    ");
+    for (int i = 0; i < n; ++i)
+       System.out.print(i + " ");
+       System.out.println("\n");
+    for (int i = 0; i < n; ++i) {
+    System.out.print(i + "   ");
+	for (int j = 0; j < n; ++j)
+	    System.out.print((tablero[i] == j ? "Q" : "#") + " ");
+	    System.out.println();
+	}
+    System.out.println();
+  }
     
 }
